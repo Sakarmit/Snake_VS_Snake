6d9e9a30e52e3629bba84d4ecf01e46c4f9832c7
Sakarmit <40968890+Sakarmit@users.noreply.github.com>
2024-10-26 22:16:22 -0400

    Resolved spawn teleport bug on bottom border

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -23,6 +23,7 @@ public class Snake : MonoBehaviour
     // Positioning/Movement related vars
     Vector3 expectedGridPosition, nextPosition;
     private float satisfactionDistance = 0.09f;
+    float loopTolerance = 0.02f;
 
     // Snake Info
     [SerializeField] GameObject bodyPrefab;
@@ -94,15 +95,17 @@ public class Snake : MonoBehaviour
     void loopToBoard() {
         //If the snake is off grid move it back on the other size relatively
         Vector3 newPos = transform.position;
-        if (newPos.x > xRange[1]) {
+        if (newPos.x - xRange[1] > loopTolerance) {
+            print(newPos.x - xRange[1]);
             newPos.x = ((newPos.x + xRange[0])%(2*xRange[1]))+xRange[0]-0.5f;
-        } else if (newPos.x < xRange[0]) {
+        } else if (xRange[0] - newPos.x > loopTolerance) {
             newPos.x = ((newPos.x + xRange[1])%(2*xRange[1]))+xRange[1]+0.5f;
         } 
 
-        if (newPos.y > yRange[1]) {
+        if (newPos.y - yRange[1] > loopTolerance) {
             newPos.y = ((newPos.y + yRange[0])%(2*yRange[1]))+yRange[0]-0.5f;
-        } else if (newPos.y < yRange[0]) {
+        } else if (yRange[0] - newPos.y > loopTolerance) {
+            print($"B: {newPos.y} < {yRange[0]} - {newPos.y < yRange[0]}");
             newPos.y = ((newPos.y + yRange[1])%(2*yRange[1]))+yRange[1]+0.5f;
         } 
 
